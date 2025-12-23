cccba601785bfb002032c2ed542d80947be93105
Le Nhut Anh <nhutanhle130403@gmail.com>
2025-12-23 13:34:26 +0700

    Setup MongoDB connection

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MongoDB Connected Success");
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+
+}
+export default connectDB;
---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+
+dotenv.config();
+const app = express();
+connectDB();
+app.use(express.json());
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+  console.log('👀 Morgan log is ENABLED (Development Mode)');
+}
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
