eb33e0865f5efd581cef4e49b23a0d9b2763488a
Le Nhut Anh <nhutanhle130403@gmail.com>
2025-12-23 19:29:20 +0700

    add brand  model with mongoose schema

---
+++ models/Brand.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+
+const brandSchema = new mongoose.Schema({
+    name :{
+        type:String,
+        required: [true, "Vui lòng nhập tên thương hiệu"],
+        trim: true,
+        unique: true,
+    },
+    slug:{
+        type: String,
+        lowercase: true,
+        unique: true,
+        index: true,
+    },
+    description:{
+        type:String,
+        maxLength:[500, "Mô tả không được quá 500 ký tự"]
+    },
+    isActive:{
+        type: Boolean,
+        default: true,
+    },
+    isDelete: {
+      type: Boolean,
+      default: false,
+    },
+    deleteAt: {
+      type: Date,
+      default: null,
+    }
+},{timestamps:true});
+brandSchema.pre("save", async function () {
+  if (this.isModified("name")) {
+    this.slug = this.name.split(" ").join("-").toLowerCase();
+  }
+});
+const Brand = brandSchema.model("Brand", brandSchema);
+export default Brand;
